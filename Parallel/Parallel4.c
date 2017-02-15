8022ac4e34ac9f438d01c75bf8c412c0ef750715
Sagar Kalbande <sagarkalbande1993@gmail.com>
2017-02-16 01:10:04 +0530

    Update Parallel4.c

--- Parallel/Parallel4.c
+++ Parallel/Parallel4.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 #include<omp.h>
 
-
-
 int main() {
+
+	double startTime = 0, runTime = 0;
+	startTime = omp_get_wtime();
 	float no_of_steps = 1000;
 	float step_size = 1 / no_of_steps;
 	float x = 0;
 	float y_value = 0;
 	float area = 0;
-
+//Try changing number of threads and experimenting with the runtime
+	omp_set_num_threads(64);
 #pragma omp parallel
 	{
 		
@@ -22,9 +24,9 @@ int main() {
 			printf("Thread number %d calculated area as :%f\n", omp_get_thread_num() ,area);
 		}
 	}
-	printf("%f", 4 * area);
 
+	runTime = omp_get_wtime() - startTime;
+	printf("%f %lf", 4 * area, runTime);
 	getchar();
 	return 0;
-
 }
