e0c750cbb4f62c38ff10dfc55d2447310b948e45
Sagar Kalbande <sagarkalbande1993@gmail.com>
2017-02-16 00:57:00 +0530

    Update Parallel4.c

--- Parallel/Parallel4.c
+++ Parallel/Parallel4.c
@@ -1,26 +1,30 @@
-/*   This shows area as 0 for all threads!! Why??*/
-
 #include<stdio.h>
 #include<omp.h>
 
+
+
 int main() {
-	const int no_of_steps = 10;
+	float no_of_steps = 1000;
 	float step_size = 1 / no_of_steps;
 	float x = 0;
 	float y_value = 0;
-	float area[no_of_steps];
-	int numofthreads;
-	float sum = 0;
+	float area = 0;
 
-	omp_set_num_threads(no_of_steps) ;
-	#pragma omp parallel shared(area)
+#pragma omp parallel
 	{
-		numofthreads = omp_get_num_threads();
-		int ID = omp_get_thread_num();
-		area[ID] = step_size*(4 / (1 + step_size*ID*step_size*ID));
-		printf("%d %f\n", ID, area);
-	};
+		
+#pragma omp for
+		for (int i = 0; i < (int)no_of_steps; i++)
+		{
+			x = x + step_size;
+			y_value = 1 / (1 + x*x);
+			area = area + (y_value)*step_size;
+			printf("Thread number %d calculated area as :%f\n", omp_get_thread_num() ,area);
+		}
+	}
+	printf("%f", 4 * area);
 
-
+	getchar();
 	return 0;
+
 }
